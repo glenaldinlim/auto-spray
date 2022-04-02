f753a8e14878eb038ae6e90063442efc1b4c348c
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2022-04-02 13:20:48 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Pin Configuration
+dhtPin = 27
+limitSwitchPins = [34, 35]
+pompPins = [12, 14]
+motorPins = [16, 17]
+i2cPins = [21, 22]
+
+# Access Point Configuration
+ap_ssid = "AutoSpray-AP"
+ap_password = "TugasAkhir"
+ap_authmode = 3
+
+# API Configuration
+endpointUrl = "https://auto-spray-iot.vercel.app/api/"
+pathVersion = "v1"
