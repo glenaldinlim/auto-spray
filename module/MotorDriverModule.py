b3c08c9a11ba5fa0b274575dd2da9aae0129f9e9
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2022-04-11 09:33:23 +0700

    Update MotorDriverModule.py

--- module/MotorDriverModule.py
+++ module/MotorDriverModule.py
@@ -1,10 +1,8 @@
-from machine import Pin, PWM
+from machine import Pin
 
-def MotorDriverControl(pins, states, pwm_duty = 1023):
+def MotorDriverControl(pins, states):
     out1 = Pin(pins[0], Pin.OUT)
     out2 = Pin(pins[1], Pin.OUT)
-    enab = PWM(Pin(pins[2]))
     
     out1.value(states[0])
     out2.value(states[1])
-    enab.duty(pwm_duty)
