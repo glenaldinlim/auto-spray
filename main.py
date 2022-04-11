9e7189a57443f0eece31cff27569607bcce206fc
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2022-04-11 12:29:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,47 @@ from utime import sleep
 from DHTModule import ReadValue
 from MotorDriverModule import MotorDriverControl
 from LimitSwitchModule import ReadSwitchValue
-from WifiManagerModule import openWifiManager
+# from WifiManagerModule import openWifiManager
+from CommuModule import changeMode, receiveDetection
+from APIModule import getDataAPI, postDataAPI
+from FirebaseModule import getData, storeData, putData, deleteData
+from WiFiConnectionModule import WiFiConnection
 
 # Initialize
 print("Initialize System...")
 # openWifiManager()
+WiFiConnection()
+resetQueue = getData("/monitor")
+print(resetQueue)
 print("Starting System in 5 Second...")
 sleep(5)
 
 # Main Loop
-while True:
-    if(ReadSwitchValue(1) == 1):
-        MotorDriverControl(motorPins[:2], [1, 0])
-    elif(ReadSwitchValue(2) == 1):
-        MotorDriverControl(motorPins[:2], [1, 0])
-    else:
-        MotorDriverControl(motorPins[:2], [0, 0])
+# while True:
+#     print("--------------------")
+#     print(changeMode(1,1))
+#     print(receiveDetection())
+#     print("--------------------")
+#     sleep(2)
+#     MotorDriverControl(motorPins[:2], [1, 0])
+#     MotorDriverControl(pompPins[:2], [1,0])
+#     sleep(3.2)
+#     MotorDriverControl(motorPins[:2], [0, 0])
+#     MotorDriverControl(pompPins[:2], [0,0])
+#     sleep(2)
+#     MotorDriverControl(motorPins[:2], [0, 1])
+#     sleep(3.2)
+#     MotorDriverControl(motorPins[:2], [0, 0])
+#     sleep(2)
+#     print(ReadSwitchValue(1))
+#     print(ReadSwitchValue(2))
+#     print(ReadSwitchValue(0))
+#     sleep(5)
+#     if(ReadSwitchValue(1) == 1):
+#         MotorDriverControl(motorPins[:2], [0, 0])
+#     elif(ReadSwitchValue(2) == 1):
+#         MotorDriverControl(motorPins[:2], [0, 0])
+#     else:
+#         print("Fck")
+#         MotorDriverControl(motorPins[:2], [1, 0])
 
