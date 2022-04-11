58b51352cde377f713b7c618aa504458c414ee86
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2022-04-11 09:34:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ dhtPin = 27
 limitSwitchPins = [34, 35]
 pompPins = [12, 14]
 motorPins = [16, 17]
+gpioPins = [26, 25, 33, 32]
 i2cPins = [21, 22]
 
 # Access Point Configuration
@@ -13,3 +14,4 @@ ap_authmode = 3
 # API Configuration
 endpointUrl = "https://auto-spray-iot.vercel.app/api/"
 pathVersion = "v1"
+headers = {'content-type': 'application/json'}
