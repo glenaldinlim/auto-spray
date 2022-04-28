165c2cb9760d7c18b40a9ad74e8ac47e045612c1
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2022-04-28 13:46:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ from LimitSwitchModule import ReadSwitchValue
 # from WifiManagerModule import openWifiManager
 from CommuModule import changeMode, receiveDetection, readState
 from APIModule import getDataAPI, postDataAPI
-from FirebaseModule import getData, storeData, putData, deleteData
+# from FirebaseModule import getData, storeData, putData, deleteData
 from WifiConnectionModule import WiFiConnection
 
 # Initialize
-print("[SYSTEM] Initialize")
-print("[SYSTEM] Wifi Connection")
+# print("[SYSTEM] Initialize")
+# print("[SYSTEM] Wifi Connection")
 # openWifiManager()
-WiFiConnection()
+# WiFiConnection()
 
-print("[SYSTEM] Get Reset Queue Value and Store")
-monitor = getDataAPI("/realtime/monitor")
-resetQueue = monitor["data"]["reset"]
+# print("[SYSTEM] Get Reset Queue Value and Store")
+# monitor = getDataAPI("/realtime/monitor")
+# resetQueue = monitor["data"]["reset"]
 
-print("Starting System in 5 Second...")
-sleep(5)
+# print("Starting System in 5 Second...")
+# sleep(5)
 
 detect = 0
 
@@ -37,34 +37,59 @@ while True:
     sleep(2)
     changeMode(3,0)
     sleep(2)
-    while (readState() == 0):
-        if readState() == 1:
-            pass
-    config = getDataAPI("/realtime/config")
-    modeJetson = config["data"]["mode"]
-    changeMode(modeJetson,1)
-    sleep(2)
-    changeMode(3,0)
-    sleep(2)
-    detect = 0
-    while (readState() == 0):
-        if readState() == 1:
-            pass
     while(readState() == 1):
-        if(ReadSwitchValue(1) == 1):
-            MotorDriverControl(motorPins[:2], [0, 0])
-            sleep(3)
-            MotorDriverControl(motorPins[:2], [0, 1])
-        elif(ReadSwitchValue(2) == 1):
-            MotorDriverControl(motorPins[:2], [0, 0])
-            sleep(3)
-            MotorDriverControl(motorPins[:2], [0, 1])
-        else:
-            MotorDriverControl(motorPins[:2], [1, 0])
+        print("While Scope")
+#         MotorDriverControl(motorPins[:2], [1, 0])
+#         if(ReadSwitchValue(1) == 1):
+#             MotorDriverControl(motorPins[:2], [0, 0])
+#             sleep(3)
+#             MotorDriverControl(motorPins[:2], [0, 1])
+#         elif(ReadSwitchValue(2) == 1):
+#             MotorDriverControl(motorPins[:2], [0, 0])
+#             sleep(3)
+#             MotorDriverControl(motorPins[:2], [0, 1])
+#         else:
+#             MotorDriverControl(motorPins[:2], [1, 0])
         if(receiveDetection() == 1):
             detect+=1
-            MotorDriverControl(motorPins[:2], [1, 0])
-            sleep(3)
-            MotorDriverControl(motorPins[:2], [1, 0])
+            print(detect)
+            MotorDriverControl(pompPins[:2], [1, 0])
+            sleep(1)
+        else:
+            MotorDriverControl(pompPins[:2], [0, 0])
+            sleep(1)
+        sleep(0.5)
     print(detect)
-    break
+
+#     while (readState() == 0):
+#         if readState() == 1:
+#             pass
+#     config = getDataAPI("/realtime/config")
+#     modeJetson = config["data"]["mode"]
+#     changeMode(modeJetson,1)
+#     sleep(2)
+#     changeMode(3,0)
+#     sleep(2)
+#     detect = 0
+#     while (readState() == 0):
+#         if readState() == 1:
+#             pass
+#     while(readState() == 1):
+#         if(ReadSwitchValue(1) == 1):
+#             MotorDriverControl(motorPins[:2], [0, 0])
+#             sleep(3)
+#             MotorDriverControl(motorPins[:2], [0, 1])
+#         elif(ReadSwitchValue(2) == 1):
+#             MotorDriverControl(motorPins[:2], [0, 0])
+#             sleep(3)
+#             MotorDriverControl(motorPins[:2], [0, 1])
+#         else:
+#             MotorDriverControl(motorPins[:2], [1, 0])
+#         if(receiveDetection() == 1):
+#             detect+=1
+#             MotorDriverControl(motorPins[:2], [1, 0])
+#             sleep(3)
+#             MotorDriverControl(motorPins[:2], [1, 0])
+#     print(detect)
+#     break
+    
